7a40df3b355cfea0fa9392f7e17c31a0619f9efc
ArnoldA123 <chombasuarez2003@hotmail.com>
2023-05-24 12:56:46 -0500

    Agregar archivos de proyecto.

---
+++ Mantenedor formaPago.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_SANNA
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
